0862a0c3ecaa9c8789a3aecfc4b67b1c4956745e
francesco.miliotti <francesco.miliotti@email.com>
2026-03-24 23:20:33 +0100

    rotta index

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -2,6 +2,18 @@ const db = require ("../data/db");
 
 function index(eq, res) {
 
+    const sqlQuery = "SELECT * FROM movies";
+    db.query(sqlQuery, (err, result) => {
+        if (err) {
+            return res.status(500).json({
+                error: "Database error",
+                message: err.message
+            })
+        }
+
+        res.json(result);
+    })
+
 }
 
 function show(eq, res){
